271448d988b6478d6c9d60080f60d9665b65e880
Braden Watkins <bradencwatkins@gmail.com>
2024-11-04 22:14:50 -0700

    navigation is now online smile

---
+++ src/about/about.jsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+
+export function About() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>about displayed here</div>
+    </main>
+  );
+}
